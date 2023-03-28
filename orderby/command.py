0da9d288e4f6eeda78d6cca1670e0b8590c708ca
Artem Zenkov <azenkov@isgneuro.com>
2023-03-28 14:14:25 +0700

    asc argument is ALL type

--- orderby/command.py
+++ orderby/command.py
@@ -7,11 +7,15 @@ class OrderByCommand(BaseCommand):
     syntax = Syntax(
         [
             Positional(name="columns", otl_type=OTLType.TEXT, inf=True),
-            Keyword(name="asc", key="ascending", otl_type=OTLType.INTEGER, required=False)
+            Keyword(name="asc", key="ascending", otl_type=OTLType.ALL, required=False)
         ]
     )
 
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
-        asc = self.get_arg('asc').value != 0
+        asc = self.get_arg('asc').value
+        if asc is None:
+            asc = True
+        else:
+            asc = bool(asc)
         df.sort_values(by=[c.value for c in self.get_iter('columns')], axis=0, ascending=asc, inplace=True)
         return df
